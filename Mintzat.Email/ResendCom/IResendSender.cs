b0c4dc614ba1411f8d32ec4277350ad35c8184d0
Minko Staev <minkostaev@yahoo.com>
2024-11-16 19:55:54 +0200

    feat: v1.0.13 interdace

---
+++ Mintzat.Email/ResendCom/IResendSender.cs
@@ -0,0 +1,8 @@
+﻿namespace Mintzat.Email.ResendCom;
+
+public interface IResendSender
+{
+    Task<(bool, string)> SendEmail(string senderEmail, string[] recipients, string topic, string content,
+         string? replyTo = null, string[]? ccEmails = null, string[]? bccEmails = null,
+         Dictionary<string, string>? attachedFiles = null, string senderName = "");
+}
