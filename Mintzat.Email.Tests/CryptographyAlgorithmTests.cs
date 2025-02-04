16acbe8ba007dd88d9c5c301561b4ddd51beeee8
Minko Staev <minkostaev@yahoo.com>
2025-02-05 01:52:41 +0200

    feat: v1.0.16 cryptography

---
+++ Mintzat.Email.Tests/CryptographyAlgorithmTests.cs
@@ -0,0 +1,40 @@
+﻿namespace Mintzat.Email.Tests;
+
+using Mintzat.Email.Services;
+
+public class CryptographyAlgorithmTests
+{
+    private CryptographyAlgorithm _algorithm;
+
+    [SetUp]
+    public void Setup()
+    {
+        _algorithm = new CryptographyAlgorithm();
+    }
+
+    [Test]
+    public void EncryptDecrypt()
+    {
+        string textToEncrypt = "text";
+        string key = "key";
+        string encrypted = _algorithm.EncryptPlainTextToCipherText(textToEncrypt, key, out string output);
+        string result = _algorithm.DecryptCipherTextToPlainText(encrypted, key, output);
+        Assert.That(textToEncrypt, Is.EqualTo(result));
+    }
+
+    [Test]
+    public void Hash()
+    {
+        string textToEncrypt = "text";
+        string salt = "salt";
+        string result = CryptographyAlgorithm.HashString(textToEncrypt, salt);
+        Assert.That(result, Is.EqualTo("3353E16497AD272FEA4382119FF2801E54F0A4CF2057F4E32D00317BDA5126C3"));
+    }
+    [Test]
+    public void HashEmpty()
+    {
+        string result = CryptographyAlgorithm.HashString(string.Empty);
+        Assert.That(result, Is.Empty);
+    }
+
+}
---
+++ Mintzat.Email/Services/CryptographyAlgorithm.cs
@@ -0,0 +1,95 @@
+﻿namespace Mintzat.Email.Services;
+
+using System.Security.Cryptography;
+using System.Text;
+
+public class CryptographyAlgorithm
+{
+    public CryptographyAlgorithm()
+    {
+        _cipherMode = CipherMode.CBC;
+    }
+
+    #region public
+
+    public static string HashString(string text, string salt = "")
+    {
+        if (string.IsNullOrEmpty(text))
+        {
+            return string.Empty;
+        }
+        // Uses SHA256 to create the hash
+        using (var sha = SHA256.Create())
+        {
+            // Convert the string to a byte array first, to be processed
+            byte[] textBytes = Encoding.UTF8.GetBytes(text + salt);
+            byte[] hashBytes = sha.ComputeHash(textBytes);
+
+            // Convert back to a string, removing the '-' that BitConverter adds
+            string hash = BitConverter.ToString(hashBytes).Replace("-", string.Empty);
+            return hash;
+        }
+    }
+
+    public string EncryptPlainTextToCipherText(string plainText, string securityKey, out string vectorAlgorithm)
+    {
+        byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(plainText);
+        var encryption = Encrypt(toEncryptArray, CreateKey(securityKey), out byte[] iv);
+        vectorAlgorithm = Convert.ToBase64String(iv, 0, iv.Length);
+        return Convert.ToBase64String(encryption, 0, encryption.Length);
+    }
+
+    public string DecryptCipherTextToPlainText(string cipherText, string securityKey, string vectorAlgorithm)
+    {
+        byte[] toEncryptArray = Convert.FromBase64String(cipherText);
+        byte[] iv = Convert.FromBase64String(vectorAlgorithm);
+        var newDecrypt = Decrypt(toEncryptArray, CreateKey(securityKey), iv!);
+        return UTF8Encoding.UTF8.GetString(newDecrypt);
+    }
+
+    #endregion
+
+    #region private
+
+    private static byte[] CreateKey(string key)
+    {
+        var objMD5CryptoService = MD5.Create();
+        //Getting the bytes from the Security Key and Passing it to compute the Corresponding Hash Value.
+        byte[] securityKeyArray = objMD5CryptoService.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+        //De-allocatinng the memory after doing the Job.
+        objMD5CryptoService.Clear();
+        return securityKeyArray;
+    }
+
+    // https://www.meziantou.net/cryptography-in-dotnet.htm
+    private readonly CipherMode _cipherMode;
+    private byte[] Encrypt(byte[] data, byte[] key, out byte[] iv)
+    {
+        using var aes = Aes.Create();
+        // You should adjust the mode depending on what you want to encrypt.
+        // However, some mode may be weak or require additional security steps such as CBC:
+        // https://learn.microsoft.com/en-us/dotnet/standard/security/vulnerabilities-cbc-mode?WT.mc_id=DT-MVP-5003978
+        aes.Mode = _cipherMode;
+        aes.Key = key;
+        aes.GenerateIV();// You must use a new IV for each encryption for security purpose
+        using (var cryptoTransform = aes.CreateEncryptor())
+        {
+            iv = aes.IV;
+            return cryptoTransform.TransformFinalBlock(data, 0, data.Length);
+        }
+    }
+    private byte[] Decrypt(byte[] data, byte[] key, byte[] iv)
+    {
+        using var aes = Aes.Create();
+        aes.Mode = _cipherMode;
+        aes.Key = key;
+        aes.IV = iv;
+        using (var cryptoTransform = aes.CreateDecryptor())
+        {
+            return cryptoTransform.TransformFinalBlock(data, 0, data.Length);
+        }
+    }
+
+    #endregion
+
+}
